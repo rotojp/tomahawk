baffa070880ec45fd18d3d4689500ea4c478391c
Dominik Schmidt <dev@dominik-schmidt.de>
2014-12-10 19:59:34 +0100

    Add configTest to ResolverAccounts

--- data/js/tomahawk.js
+++ data/js/tomahawk.js
@@ -185,6 +185,13 @@ var TomahawkResolver = {
     },
     collection: function () {
         return {};
+    },
+    _testConfig: function (config) {
+        return Promise.resolve(this.testConfig(config)).then(function() {
+            return { result: Tomahawk.ConfigTestResultType.Success };
+        });
+    },
+    testConfig: function () {
     }
 };
 
@@ -593,4 +600,16 @@ Tomahawk.PluginManager = {
             });
         });
     }
+};
+
+
+Tomahawk.ConfigTestResultType = {
+    Other: 0,
+    Success: 1,
+    Logout: 2,
+    CommunicationError: 3,
+    InvalidCredentials: 4,
+    InvalidAccount: 5,
+    PlayingElsewhere: 6,
+    AccountExpired: 7
 };
